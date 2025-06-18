121061d4dbc5784e81290baa5973efa1486fcfe8
NatannnM <nata_martins@outlook.com>
2025-06-17 21:42:11 -0300

    regras de negócio

--- src/diretor/diretor.service.ts
+++ src/diretor/diretor.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { DiretorEntity } from "./diretor.entity";
 import { Repository } from "typeorm";
@@ -28,19 +28,54 @@ export class DiretorService{
     }
 
     async create(diretorDto: DiretorDto){
+        await this.validateBusinessRules(diretorDto);
         const diretor = this.diretorRepository.create(diretorDto);
         return this.diretorRepository.save(diretor);
     }
     
     async update(diretorId: string, diretorDto: DiretorDto){
+        await this.validateBusinessRules(diretorDto, diretorId);
         const diretor = await this.findById(diretorId);
         Object.assign(diretor, diretorDto);
         return this.diretorRepository.save(diretor);
     }
 
     async remove(diretorId: string){
+        await this.validateRemoveDiretor(diretorId);
         const diretor = await this.findById(diretorId);
         await this.diretorRepository.remove(diretor);
         return {...diretor, diretorId};
+    }
+
+    private async validateBusinessRules(diretorDto: DiretorDto, idToIgnore?: string){
+            await this.validateDateDiretor(diretorDto);
+            await this.validateFilmesUnicos(diretorDto.filmes);
+    }
+
+    private async validateDateDiretor(diretorDto: DiretorDto){
+        if (new Date(diretorDto.birthDate) > new Date()) {
+            throw new BadRequestException('A data de nascimento do diretor não pode ser futura.');
+        }
+    }
+
+    private async validateRemoveDiretor(diretorId: string){
+        const diretor = await this.diretorRepository.findOne({
+            where: { id: diretorId },
+            relations: ['filmes']
+        });
+        if(diretor?.filmes && diretor.filmes.length > 0){
+            throw new BadRequestException('Não é possível excluir um diretor associado a algum filme.');
+        }
+    }
+
+    private validateFilmesUnicos(filmes: {id: string}[] | undefined) {
+    if (!filmes || filmes.length === 0) return;
+
+    const id = filmes.map(f => f.id);
+    const set = new Set(id);
+
+    if (set.size !== id.length) {
+      throw new BadRequestException('O diretor não pode estar vinculado mais de uma vez ao mesmo filme.');
+    }
     }
 }
